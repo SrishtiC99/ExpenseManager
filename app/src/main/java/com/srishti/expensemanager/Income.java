6521af9b2c3f0539378e68cdb15886e6b08ee746
Srishti Kumari <sri993951@gmail.com>
2021-10-16 11:17:34 +0530

    Income class added

---
+++ app/src/main/java/com/srishti/expensemanager/Income.java
@@ -0,0 +1,34 @@
+package com.srishti.expensemanager;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.text.DateFormat;
+
+@Entity(tableName = "income_table")
+public class Income {
+    @PrimaryKey(autoGenerate = false)
+    private String id;
+
+    private DateFormat date;
+
+    private double value;
+
+    public Income(DateFormat date, double value) {
+        this.date = date;
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public DateFormat getDate() {
+        return date;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+}
